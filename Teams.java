401b2d46ecc1e5d0c44990395ef89ad01fc1424c
SaulPena1 <48969421+SaulPena1@users.noreply.github.com>
2019-04-17 19:59:11 -0400

    Add files via upload

--- Teams.java
+++ Teams.java
@@ -1,18 +1,32 @@
-public class Teams {
-    private String[] a;
-    private int nElems;
 
-    public Teams(String names) {
-        a = new String[10];
-    }
-
-    public void insert(String value) {
-        a[nElems] = value;
-        nElems++;
-     }
-
-     //show team names
-     public void display() {
-        for(int i = 0; i<nElems; i++)  
-  }
+public class Teams 
+{
+   private String[] a;
+   private int nElems = 0;
+   private String names;
+   public Teams() 
+   {
+      a = new String[10];
+   }
+   public void insert(String value)
+   {
+      a[nElems] = value;
+      nElems++;
+   }
+  //show team names
+   public String display() 
+   {
+      names = a[0];
+      if(a[1]==null)
+      {
+         return names;
+      }else
+      {
+      for(int i = 1; i<nElems; i++)
+      {
+         names = (names + " " + a[i]);
+      }
+      return names;
+      }  
+   }
 }
