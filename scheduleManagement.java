e42cf75b6d1b7d087dc24f4a90ed1008d1777112
hi-erick <45538635+hi-erick@users.noreply.github.com>
2019-04-17 17:34:48 -0400

    Update scheduleManagement.java

--- scheduleManagement.java
+++ scheduleManagement.java
@@ -1,5 +1,38 @@
-public class scheduleManagement {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class scheduleManagement extends JFrame implements ActionListener {
+    
+    //JButton enter = new JButton("Enter");
+    //JButton done = new JButton("Done");
+    //JTextField input = new JTextField(20);
+    JButton teamNames = new JButton("Enter Team Names");
+    //String[] options = {"Enter", "Done"};
     public static void main(String[] args) {
-        Teams array = new Teams(35);
+        Teams array = new Teams();
+        scheduleManagement frame = new scheduleManagement();
+        frame.setSize(400,200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public scheduleManagement() {
+        super();
+        setLayout(new FlowLayout() );
+        add(teamNames);
+        teamNames.addActionListener(this);        
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getScource();
+
+        if(source == teamNames) {
+            while(true) {
+                String input = JOptionPane.showInputDialog("Enter team names: (click cancel once complete)");
+                array.insert(input);
+            }
+        }
     }
 }
