7ce01c4f97122793b6aa4e184af755f5beb79dbe
hi-erick <45538635+hi-erick@users.noreply.github.com>
2019-04-18 00:43:03 -0400

    Update Teams.java

--- Teams.java
+++ Teams.java
@@ -1,32 +1,24 @@
 
-public class Teams 
-{
+import javax.swing.*;
+import java.util.*;
+
+public class Teams {
+
    private String[] a;
    private int nElems = 0;
    private String names;
-   public Teams() 
-   {
+
+   Teams() {
       a = new String[10];
    }
-   public void insert(String value)
-   {
+
+   public void insert(String value) {
       a[nElems] = value;
       nElems++;
    }
+
   //show team names
-   public String display() 
-   {
-      names = a[0];
-      if(a[1]==null)
-      {
-         return names;
-      }else
-      {
-      for(int i = 1; i<nElems; i++)
-      {
-         names = (names + " " + a[i]);
-      }
-      return names;
-      }  
+    public void display() {
+        JOptionPane.showMessageDialog(null, Arrays.toString(a));
    }
 }
