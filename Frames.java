998c1524b720e50d20898786adde1a3c44dc28c7
hi-erick <45538635+hi-erick@users.noreply.github.com>
2019-04-22 22:26:01 -0400

    Update Frames.java

--- Frames.java
+++ Frames.java
@@ -2,18 +2,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-class Frames implements ActionListener{
-    static JButton teamA = new JButton("Team A");
-    static JButton teamB = new JButton("Team B");
-    static JButton teamC = new JButton("Team C");
-    static JButton teamD = new JButton("Team D");
-    static JButton teamE = new JButton("Team E");
-    static JButton teamF = new JButton("Team F");
-    static JButton teamG = new JButton("Team G");
-    static JButton teamH = new JButton("Team H");
-    static JButton teamI = new JButton("Team I");
-    static JButton teamJ = new JButton("Team J");
-    public static void main(String [] args) {
+class Frames implements ActionListener {
+    
+    public void main(String [] args) {
+       
+        Container con = getContentPane();
+        JButton teamA = new JButton("Team A");
+        JButton teamB = new JButton("Team B");
+        JButton teamC = new JButton("Team C");
+        JButton teamD = new JButton("Team D");
+        JButton teamE = new JButton("Team E");
+        JButton teamF = new JButton("Team F");
+        JButton teamG = new JButton("Team G");
+        JButton teamH = new JButton("Team H");
+        JButton teamI = new JButton("Team I");
+        JButton teamJ = new JButton("Team J");
+        
         JRadioButton win = new JRadioButton("Won");
         JRadioButton loss = new JRadioButton("Loss");
         JRadioButton draw = new JRadioButton("Draw");
@@ -29,30 +33,41 @@ class Frames implements ActionListener{
         JFrame frame9 = new JFrame("Team I");
         JFrame frame10 = new JFrame("Team J");
 
-        frame.add(win);
-        frame.add(loss);
-        frame.add(draw);
+        /*static*/ boolean visible = false;
+
+
+
+        //frame.add(win);
+        //frame.add(loss);
+        //frame.add(draw);
         frame.setSize(700,500);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame1.setSize(500,500);
+        frame1.setLocationRelativeTo(null);
+        frame1.setVisible(visible);;
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        con.add(frame);
+        //frame.setLayout(new FlowLayout());
+        frame.add(teamA);
         teamA.addActionListener(this);
-        teamB.addActionListener(this);
-        teamC.addActionListener(this);
-        teamD.addActionListener(this);
-        teamE.addActionListener(this);
-        teamF.addActionListener(this);
-        teamG.addActionListener(this);
-        teamH.addActionListener(this);
-        teamI.addActionListener(this);
-        teamJ.addActionListener(this);
+        //teamB.addActionListener(this);
+        //teamC.addActionListener(this);
+        //teamD.addActionListener(this);
+        //teamE.addActionListener(this);
+        //teamF.addActionListener(this);
+        //teamG.addActionListener(this);
+        //teamH.addActionListener(this);
+        //teamI.addActionListener(this);
+        //teamJ.addActionListener(this);
     }
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         
         if(source == teamA) {
-            
+            visible = true;
         }
     }
 }
